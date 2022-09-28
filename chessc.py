b740985c75c1d0d5f190e47aea1aa31db34ff109
proarche <91221220+proarche@users.noreply.github.com>
2022-09-29 00:06:32 +0200

    Add files via upload

---
+++ chessc.py
@@ -0,0 +1,70 @@
+import sys
+from pathlib import Path
+import chess
+import chess.engine
+import chess.pgn
+import chess.polyglot
+
+STOCKFISH_PATH = "./engines/stockfish_15_linux_x64/stockfish_15_x64"
+KOMODO_PATH    = "./engines/komodo-13_201fd6/Linux/komodo-13.02-linux"
+BOOK_PATH      = "./books/komodo.bin"
+ENGINE_PATHS   = [STOCKFISH_PATH, KOMODO_PATH]
+
+def main(thinking_time, pgn):
+    book = chess.polyglot.open_reader(BOOK_PATH)
+
+    engines = []
+    for engine_path in ENGINE_PATHS:
+        engines.append(chess.engine.SimpleEngine.popen_uci(engine_path))
+  
+    if (Path(pgn).is_dir()):
+        analyze_folder(engines, thinking_time, book, pgn)
+    else:
+        analyze_file(engines, thinking_time, book, pgn)
+    
+    for engine in engines:
+        engine.quit()
+
+def analyze_folder(engines, thinking_time, book, pgn_dir):
+    for pgn_path in Path(pgn_dir).glob("*.pgn"):    
+        analyze_file(engines, thinking_time, book, pgn_path)
+
+def analyze_file(engines, thinking_time, book, pgn_path):
+    pgn = open(pgn_path)
+    (white, black) = calculate_correlation(engines, thinking_time, book, pgn)
+
+    print(pgn_path)      
+    print("White: " + str(white) + "%")
+    print("Black: " + str(black) + "%")
+
+def calculate_correlation(engines, thinking_time, book, pgn):
+    game  = chess.pgn.read_game(pgn)
+    board = game.board()
+
+    best_white  = []
+    best_black  = []
+
+    for move in game.mainline_moves():
+        if(book.get(board) != None):
+            board.push(move)
+            continue
+    
+        engine_moves = [] 
+        for engine in engines:
+            engine_moves.append(engine.play(board, chess.engine.Limit(time=thinking_time)).move)
+
+        if (board.turn == chess.WHITE):
+            best_white.append(move in engine_moves)
+        else:
+            best_black.append(move in engine_moves)
+
+        board.push(move)
+
+    return (round(100*(sum(best_white)/len(best_white)), 2), round(100*(sum(best_black)/len(best_black)), 2))
+
+
+if (len(sys.argv) == 3):
+    main(float(sys.argv[1]), sys.argv[2])
+else:
+    print("Wrong number of arguments")
+    print("Usage: " + sys.argv[0] + " [analysis time (in seconds) per move] [pgn folder/pgn file]")
